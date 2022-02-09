7a419424213262a46edc186dbcdf47055f2ce5fc
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-09 14:46:49 -0900

    add quantity form
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

---
+++ src/components/StoreDisplay/CartPane/CartQtyForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CartQtyForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={null}>
+        <input />
+        <button type='submit'>Change Qty</button>
+      </form>
+    </React.Fragment>
+  )
+}
+
+export default CartQtyForm;
