7978a62eb99e5ad714767643d763a75b5e04c87a
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-09 14:47:48 -0900

    add product select pane
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

---
+++ src/components/StoreDisplay/ProductSelectPane/ProductSelectPane.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ProductSelectItem from './ProductSelectItem.js'
+
+function ProductSelectPane(props){
+  return (
+      <ProductSelectItem/>
+  );
+}
+
+export default ProductSelectPane;
