57ee25297010e5a4b1f68cd6e55856bdc3cd9410
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-09 14:44:33 -0900

    add quantity form
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

---
+++ src/components/InventoryManagementDisplay/ProductStockQtyForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ProductStockQtyForm() {
+  return (
+    <React.Fragment>
+      <form onSubmit={null}>
+        <input />
+        <button type='submit'>Add Stock</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+export default ProductStockQtyForm;
