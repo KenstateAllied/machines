9bfdf90e5534d9b0a664fc71839460d2a142eca0
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-09 14:49:04 -0900

    add add to cart form

---
+++ src/components/StoreDisplay/SelectedProductPane/AddToCartForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function AddToCartForm(props){
+  return (
+    <React.Fragment>
+      <form onSubmit={null}>
+        <input/>
+        <button></button>
+      </form>
+    </React.Fragment>
+  )
+}
+
+export default AddToCartForm;
