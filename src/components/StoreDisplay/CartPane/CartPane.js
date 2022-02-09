b02c1c017063a8ef8c269ded59813f6742c4ee93
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-09 14:46:05 -0900

    add cart pane component
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

---
+++ src/components/StoreDisplay/CartPane/CartPane.js
@@ -0,0 +1,21 @@
+import React from "react";
+import CartItem from "./CartItem";
+import CheckoutButton from "./CheckoutButton";
+
+function CartPane(props) {
+  const CartPaneStyle = {
+    border: '1px solid orange',
+    padding: '1em'
+  }
+  return (
+    <React.Fragment>
+      <div style={CartPaneStyle}>
+        <CartItem/>
+        <h3>Total:</h3>
+        <CheckoutButton/>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default CartPane;
