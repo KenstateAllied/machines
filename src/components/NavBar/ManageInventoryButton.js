0185a8c60602dd8a4e5bc3754d66c46fa2703943
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-09 14:40:46 -0900

    add manage inventory button
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

---
+++ src/components/NavBar/ManageInventoryButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ManageInventoryButton(props) {
+  return (
+    <button>Manage Inventory</button> // Needs function that changes state
+  );
+}
+
+export default ManageInventoryButton;
