75614c256a84c11eb67e580265b1138cdce023d9
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-09 14:45:37 -0900

    add App.js
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import StateControl from "./StateControl";
+
+function App() {
+  return (
+    <React.Fragment>
+      <StateControl/>
+    </React.Fragment>
+  );
+}
+
+export default App;
