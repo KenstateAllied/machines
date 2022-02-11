fbb5ce734ad000665f14707106cc8e53e8b757a8
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-10 15:18:51 -0900

    add styling
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

--- src/components/InventoryManagementDisplay/InventoryManagementDisplay.js
+++ src/components/InventoryManagementDisplay/InventoryManagementDisplay.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import InventoryItem from "./InventoryItem";
 import AddProductButton from "./AddProductButton";
+import {v4}from "uuid";
 
 
 function InventoryManagementDisplay(props) {
+  const InventoryManagementDisplayStyle = {
+    border: '1px solid black',
+    padding: '1em'
+  }
   return (
     <React.Fragment>
-      <InventoryItem/>
-      <AddProductButton/>
+      <div style={InventoryManagementDisplayStyle}>
+        {props.stock.map(product => 
+          <InventoryItem product={product}
+          key={v4()} />
+        )}
+      </div>
+      <AddProductButton updatePage={props.updatePage}/>
     </React.Fragment>
   );
 }
