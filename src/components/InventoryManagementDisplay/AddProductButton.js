d95b1ad64956c351316f4dcb9d3c0ce521097d87
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-10 15:12:54 -0900

    add click function

--- src/components/InventoryManagementDisplay/AddProductButton.js
+++ src/components/InventoryManagementDisplay/AddProductButton.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 function AddProductButton(props){
+
+  function onClick() {
+    props.updatePage("AddProductForm");
+  };
+
   return (
-    <button>Add</button>
+    <button onClick={onClick}>Add a Product</button>
   )
 }
 
