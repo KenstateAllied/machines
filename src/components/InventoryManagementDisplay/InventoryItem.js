179aa95ae167e8bc37a54e2e3057deeb170815b1
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-10 15:16:47 -0900

    add styling
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

--- src/components/InventoryManagementDisplay/InventoryItem.js
+++ src/components/InventoryManagementDisplay/InventoryItem.js
@@ -3,12 +3,25 @@ import ProductStockQtyForm from './ProductStockQtyForm';
 import EditProductButton from './EditProductButton';
 import DeleteProductButton from './DeleteProductButton';
 
-function InventoryItem() {
+function InventoryItem(props) {
+  const InventoryItemStyle = {
+    border: '1px solid black',
+    padding: '1em'
+  }
+
   return (
     <React.Fragment>
-      <ProductStockQtyForm />
-      <EditProductButton />
-      <DeleteProductButton />
+      <div style={InventoryItemStyle}>
+        <h2>{props.product.name}</h2>
+        <h3>${props.product.price}</h3>  
+        <p>Qty in Stock: {props.product.qty}</p>
+        <div>
+          {props.product.description}
+        </div>
+        <ProductStockQtyForm />
+        <EditProductButton />
+        <DeleteProductButton />
+      </div>
     </React.Fragment>
   );
 }
