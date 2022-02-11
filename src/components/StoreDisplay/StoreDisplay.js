99004f801e5e04af9e6e09b51fe916a7dc1b6b9e
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2022-02-10 15:26:40 -0900

    add styling
    Co-Authored-by: Kim Brannian <brannian@gmail.com>
    Co-authored-by: Liz Thomas <thomas.elizabeth.k@gmail.com>
    Co-authored-by: Aaron Minnick <abminnick@gmail.com>

--- src/components/StoreDisplay/StoreDisplay.js
+++ src/components/StoreDisplay/StoreDisplay.js
@@ -1,19 +1,27 @@
 import React from "react";
 import CartPane from "./CartPane/CartPane";
 import ProductSelectPane from "./ProductSelectPane/ProductSelectPane";
-import SelectProductPane from "./SelectedProductPane/SelectedProductPane";
+import SelectedProductPane from "./SelectedProductPane/SelectedProductPane";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function StoreDisplay(props){
+  let productPane = null;
+  if (props.selectedProduct === null) {
+    productPane = <h2>Please select a product.</h2> 
+  } else {
+    productPane = <SelectedProductPane product={props.selectedProduct} />
+  }
+
   return (
     <React.Fragment>
       <Row>
         <Col className="col-3">
-          <ProductSelectPane/>
+          <ProductSelectPane categoryStock={props.categoryStock} 
+            updateSelectedProduct={props.updateSelectedProduct} />
         </Col>
         <Col className="col-6">
-          <SelectProductPane/>
+          {productPane}
         </Col>
         <Col className="col-3">
           <CartPane/>
